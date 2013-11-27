4e69b9f31891277aded8f9e9fbaa5f034a4827ae
Serban-Ciprian Toma <ursulbrumi@gmail.com>
2013-11-27 12:37:28 +0200

    added address tab to return zipcode

--- js/script.js
+++ js/script.js
@@ -26,4 +26,45 @@ $(document).ready(function() {
 			dataType: 'jsonp'
 		});
 	});
+
+    $("#btnSearchZipcode").click(function(e) {
+        e.preventDefault();
+        var location = $("#location").val();
+        var description = $("#address").val();
+        var url = 'http://openapi.ro/api/addresses.json';
+        $.ajax({
+            type: 'get',
+            url: url,
+            data: {description: description, location: location},
+            success: function(obj) {
+                var result = '';
+                if(obj[0] != undefined) {
+                    $.each(obj, function(index, data) {
+                        result += "<li>Cod Postal: " + data.zip + "</li>";
+                        result += "<li>Stradă: " + (data.street_type != '' ? data.street_type + ' ' : '') + data.description + "</li>";
+                        result += (data.sector != '' ? '<li>Sector: ' + data.sector + '</li>' : '');
+                        result += "<li>Localitate: " + data.location + "</li>";
+                        result += "<li>Județ: " + data.county + "</li>";
+                        result += "<li class='borderBottom'>&nbsp;</li>";
+                    });
+                } else {
+                    result += '<li>Nu am gasit nimic</li>';
+                }
+                $("#searchResultsAddress").empty().append(result);
+                $('#searchResultsAddress').listview('refresh');
+            },
+            dataType: 'jsonp'
+        });
+    });
+
+    $('#zipcodeTab').click(function(e){
+        e.preventDefault();
+        $('#zipcodeContainer').show();
+        $('#addressContainer').hide();
+    });
+    $('#addressTab').click(function(e){
+        e.preventDefault();
+        $('#zipcodeContainer').hide();
+        $('#addressContainer').show();
+    });
 });
